8128849b4ed34c2619fc650e8fbab3b8266cf4bd
dengyh <dyaohui08@gmail.com>
2020-08-12 13:40:46 +0800

    初始化测试

---
+++ Week1/Calculate_Function_By_Pytorch.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/8/12 10:13
+# @Author  : AiVision_YaoHui
+# @FileName: Calculate_Function_By_Pytorch.py
+import torch
+
+
+
+
+
+#使用pytorch计算L=(5x1+3x2-1)^2+(-3x1-4x2+1)^2
